2d97619020116076ebf0e2d3296699a46a057317
valentinportal <portalvalentin@gmail.com>
2023-12-11 13:14:36 +0100

    clear day11

--- src/_2023/Day11/Day11.java
+++ src/_2023/Day11/Day11.java
@@ -12,36 +12,28 @@ import java.util.Scanner;
 public class Day11 {
     static final char EMPTY_SPACE = '.';
     static final char GALAXY = '#';
-    static final int ECART = 999999;
+    static final int ECARTPT2 = 999999;
+    static final int ECARTPT1 = 1;
 
     public static void main(String[] args) {
         Scanner in;
         try {
             in = new Scanner(new File("_2023\\Day11\\input.txt"));
             ArrayList<String> lines = new ArrayList<String>();
-            ArrayList<String> linesExpend = new ArrayList<String>();
             String line = "";
 
             while (in.hasNextLine()) {
                 line = in.nextLine();
-                boolean noG = true;
-                for (char ch : line.toCharArray()) {
-                    if (ch == '#') {
-                        noG = false;
-                        break;
-                    }
-                }
-                if (noG) {
-                    linesExpend.add(line);
-                }
-                linesExpend.add(line);
                 lines.add(line);
             }
-            int solution1 = 0;
+            Long solution1 = 0l;
             Long solution2 = 0l;
+            List<Integer> posX = getPosX(lines);
+            List<Integer> posY = getPosY(lines);
+            List<int[]> posGalaxies = getGalaxie(lines);
 
-            solution1 = part1(getUnivers(linesExpend));
-            solution2 = part2(lines);
+            solution1 = part1(posX, posY, posGalaxies);
+            solution2 = part2(posX, posY, posGalaxies);
 
             System.out.println("solution 1 : " + solution1);
             System.out.println("solution 2 : " + solution2);
@@ -50,27 +42,17 @@ public class Day11 {
         }
     }
 
-    private static int part1(List<String> univers) {
-        int res = 0;
-        List<int[]> posGalaxies = getGalaxie(univers);
-        for (int i = 0; i < posGalaxies.size(); i++) {
-            int sr = posGalaxies.get(i)[0];
-            int sc = posGalaxies.get(i)[1];
-            for (int j = i + 1; j < posGalaxies.size(); j++) {
-                int x = Math.abs(posGalaxies.get(j)[0] - sr);
-                int y = Math.abs(posGalaxies.get(j)[1] - sc);
-                res += (x + y);
-            }
-        }
-
-        return res;
+    private static Long part1(List<Integer> posX, List<Integer> posY, List<int[]> posGalaxies) {
+        return getDistance(posX, posY, posGalaxies, ECARTPT1);
     }
 
-    private static Long part2(List<String> univers) {
+    private static Long part2(List<Integer> posX, List<Integer> posY, List<int[]> posGalaxies) {
+        return getDistance(posX, posY, posGalaxies, ECARTPT2);
+    }
+
+    private static Long getDistance(List<Integer> posX, List<Integer> posY, List<int[]> posGalaxies,
+            int ecart) {
         Long res = 0l;
-        List<Integer> posX = getPosX(univers);
-        List<Integer> posY = getPosY(univers);
-        List<int[]> posGalaxies = getGalaxie(univers);
         for (int i = 0; i < posGalaxies.size(); i++) {
             int sr = posGalaxies.get(i)[0];
             int sc = posGalaxies.get(i)[1];
@@ -79,17 +61,17 @@ public class Day11 {
                 int y = Math.abs(posGalaxies.get(j)[1] - sc);
                 for (int xx : posX) {
                     if (xx < posGalaxies.get(j)[0] && xx > sr) {
-                        x += ECART;
+                        x += ecart;
                     }
                 }
                 for (int yy : posY) {
                     if (posGalaxies.get(j)[1] - sc > 0) {
                         if (yy > sc && yy < posGalaxies.get(j)[1]) {
-                            y += ECART;
+                            y += ecart;
                         }
                     } else if (posGalaxies.get(j)[1] - sc < 0) {
                         if (yy > posGalaxies.get(j)[1] && yy < sc) {
-                            y += ECART;
+                            y += ecart;
                         }
                     }
                 }
@@ -106,7 +88,7 @@ public class Day11 {
         for (int c = 0; c < cols; c++) {
             boolean noG = true;
             for (int r = 0; r < rows; r++) {
-                if (univers.get(r).charAt(c) == '#') {
+                if (univers.get(r).charAt(c) == GALAXY) {
                     noG = false;
                     break;
                 }
@@ -125,7 +107,7 @@ public class Day11 {
         for (int r = 0; r < rows; r++) {
             boolean noG = true;
             for (int c = 0; c < cols; c++) {
-                if (univers.get(r).charAt(c) == '#') {
+                if (univers.get(r).charAt(c) == GALAXY) {
                     noG = false;
                     break;
                 }
@@ -147,32 +129,5 @@ public class Day11 {
             }
         }
         return res;
-    }
-
-    private static List<String> getUnivers(List<String> lines) {
-        int rows = lines.size();
-        int cols = lines.get(0).length();
-        List<Integer> ajoutC = new ArrayList<>();
-        int i = 0;
-        for (int c = 0; c < cols; c++) {
-            boolean noG = true;
-            for (int r = 0; r < rows; r++) {
-                if (lines.get(r).charAt(c) == '#') {
-                    noG = false;
-                    break;
-                }
-            }
-            if (noG) {
-                ajoutC.add(c + i);
-                i++;
-            }
-        }
-        for (Integer c : ajoutC) {
-            for (int r = 0; r < rows; r++) {
-                String test = lines.get(r);
-                lines.set(r, test.substring(0, c) + "." + test.substring(c));
-            }
-        }
-        return lines;
     }
 }
